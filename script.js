856292bf52c54bf9281c9eea57bb6f4bea5bc3aa
William <william.legal03@gmail.com>
2017-01-30 11:59:44 +0100

    refacto affichage images

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
-var imgArray = new Array();
+var t = ['img/chat.jpg','img/chien.jpg','img/ane.jpg','img/lionne.jpg','img/lama.jpg','img/lapins.jpg','img/ours.jpg'];
+// variable utilises
+var oImage, oDest, i, nb = t.length;
+// recuperation du conteneur
+oDest = document.getElementById('mosaique');
+for( i = 0; i < nb; i++){
+  // creation de l'element image
+  oImage = document.createElement('IMG');
+  // affectation du nom de l'image
+  oImage.src = t[i];
+  // ajout d'un ID pour recuperation ulterieure
+  oImage.id = 'image' +i;
+  // ajout au conteneur
 
-imgArray[0] = new Image();
-imgArray[0].src = 'img/ane.jpg';
-document.getElementById('mosaique').appendChild(imgArray[0]);
-imgArray[1] = new Image();
-imgArray[1].src = 'img/chat.jpg';
-document.getElementById('mosaique').appendChild(imgArray[1]);
-imgArray[2] = new Image();
-imgArray[2].src = 'img/chien.jpg';
-document.getElementById('mosaique').appendChild(imgArray[2]);
-imgArray[3] = new Image();
-imgArray[3].src = 'img/lama.jpg';
-document.getElementById('mosaique').appendChild(imgArray[3]);
-imgArray[4] = new Image();
-imgArray[4].src = 'img/lapins.jpg';
-document.getElementById('mosaique').appendChild(imgArray[4]);
-imgArray[5] = new Image();
-imgArray[5].src = 'img/lionne.jpg';
-document.getElementById('mosaique').appendChild(imgArray[5]);
-imgArray[6] = new Image();
-imgArray[6].src = 'img/ours.jpg';
-document.getElementById('mosaique').appendChild(imgArray[6]);
-imgArray[0] = new Image();
-imgArray[0].src = 'img/ane.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[0]);
-imgArray[1] = new Image();
-imgArray[1].src = 'img/chat.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[1]);
-imgArray[2] = new Image();
-imgArray[2].src = 'img/chien.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[2]);
-imgArray[3] = new Image();
-imgArray[3].src = 'img/lama.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[3]);
-imgArray[4] = new Image();
-imgArray[4].src = 'img/lapins.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[4]);
-imgArray[5] = new Image();
-imgArray[5].src = 'img/lionne.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[5]);
-imgArray[6] = new Image();
-imgArray[6].src = 'img/ours.jpg';
-document.getElementById('mosaique2').appendChild(imgArray[6]);
-
+  oDest.appendChild( oImage);
+}
+oDest = document.getElementById('mosaique2');
+for( i = 0; i < nb; i++){
+  // creation de l'element image
+  oImage = document.createElement('IMG');
+  // affectation du nom de l'image
+  oImage.src = t[i];
+  // ajout d'un ID pour recuperation ulterieure
+  oImage.id = 'image' +i;
+  // ajout au conteneur
+  
+  oDest.appendChild( oImage);
+}
